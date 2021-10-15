4824b922d210ad806578073a2ca9a7221d695eb3
JCarlosVico <jfervic933@maralboran.es>
2021-10-15 13:41:55 +0200

    Ejemplos if else anidados y if else if

--- src/main/java/jcarlos/NumerosPositivoNegativoCero.java
+++ src/main/java/jcarlos/NumerosPositivoNegativoCero.java
@@ -16,13 +16,30 @@ public class NumerosPositivoNegativoCero {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce un número: ");
         int numero = teclado.nextInt();
-        
-        
-        
-    }
+
+        if (numero < 0) {
+            System.out.println("Es negativo");
+        } else {
+            if (numero > 0) {
+                System.out.println("Es positivo");
+            } else {
+                System.out.println("Es cero");
+            }
+        }
+
+        if (numero < 0) {
+            System.out.println("Es negativo");
+        } else if (numero > 0) {
+            System.out.println("Es positivo");
+        } else {
+                System.out.println("Es cero");
+            }
+        }
+
     
+
 }
