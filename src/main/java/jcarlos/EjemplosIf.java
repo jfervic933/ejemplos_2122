c28c8dd990c4091f493f9ef17f2f195c8bc15aa9
JCarlosVico <jfervic933@maralboran.es>
2021-10-15 13:06:44 +0200

    Archivos iniciales del proyecto

---
+++ src/main/java/jcarlos/EjemplosIf.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jcarlos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author profesor
+ */
+public class EjemplosIf {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce tu edad: ");
+        int edad = teclado.nextInt();
+        System.out.println("Tu edad es: " + edad);
+        
+    }
+    
+}
