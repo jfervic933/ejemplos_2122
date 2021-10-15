b524cb51afd75bfe4fe91b537f3dc2106fb5fcf7
JCarlosVico <jfervic933@maralboran.es>
2021-10-15 13:28:09 +0200

    Controla que la edad esté en un rango válido

--- src/main/java/jcarlos/EjemplosIf.java
+++ src/main/java/jcarlos/EjemplosIf.java
@@ -19,9 +19,13 @@ public class EjemplosIf {
         int edad = teclado.nextInt();
         System.out.println("Tu edad es: " + edad);
         
-        if(edad<0){
+        if(edad<0 || edad > 150){
             System.out.println("La edad introducida es incorrecta");
+        } else {
+            System.out.println("La edad es correcta");
         }
+        
+     
         
     }
     
