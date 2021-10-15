47b8dc35e6ed46e20ffc2ae661374e2963a0ecea
JCarlosVico <jfervic933@maralboran.es>
2021-10-15 13:36:01 +0200

    Ejercicio de positivos, negativos y cero

---
+++ src/main/java/jcarlos/NumerosPositivoNegativoCero.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jcarlos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author profesor
+ */
+public class NumerosPositivoNegativoCero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int numero = teclado.nextInt();
+        
+        
+        
+    }
+    
+}
