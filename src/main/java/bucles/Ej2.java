5494643ed2494b67b295ab8bd75d7716c7dc8441
JCarlosVico <jfervic933@maralboran.es>
2021-10-22 13:02:52 +0200

    Ejercicio de bucles while

---
+++ src/main/java/bucles/Ej2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bucles;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author profesor
+ */
+public class Ej2 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int n, x;
+        System.out.print("Ingrese el valor final:");
+        n = teclado.nextInt();
+        x = 1;
+        while (x <= n) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+
+    }
+
+}
