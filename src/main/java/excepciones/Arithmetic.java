84655b6fac5b1e794ea2247d60b1650195a5a57a
JCarlosVico <jfervic933@maralboran.es>
2021-11-05 13:11:09 +0100

    Ejemplo try con varios catch

--- src/main/java/excepciones/Arithmetic.java
+++ src/main/java/excepciones/Arithmetic.java
@@ -15,26 +15,36 @@ public class Arithmetic {
 
     public static void main(String[] args) {
 
-        int numero1, numero2;
+        int numero1 = 0, numero2 = 0, division = 0;
         Scanner teclado = new Scanner(System.in);
+        boolean seguir = true;
+        
+        do {
+            try {
+                System.out.println("Introduce n1: ");
+                numero1 = teclado.nextInt();
 
-        try {
-            System.out.println("Introduce n1: ");
-            numero1 = teclado.nextInt();
-
-            System.out.println("Introduce n2: ");
-            numero2 = teclado.nextInt();
-
-        } catch (InputMismatchException ime){
-            System.out.println("Cuidado pecador, te has equivocado");
-        }
+                System.out.println("Introduce n2: ");
+                numero2 = teclado.nextInt();
                 
+                division = numero1 / numero2;
                 
+                seguir = false;
                 
-//        int division = numero1 / numero2;
-//
-//            System.out.println("Resultado: " + division);
+            } catch (InputMismatchException ime) {
+                System.out.println("Cuidado pecador, te has equivocado. "
+                        + "Introduce solo números");
+                teclado.nextLine();
+            } catch (ArithmeticException ae){
+                System.out.println("Estás mu tonto, no puedes dividir por cero");
+            }
+        } while (seguir);
 
-        }
+  
+        
+
+        System.out.println("Resultado: " + division);
 
     }
+
+}
