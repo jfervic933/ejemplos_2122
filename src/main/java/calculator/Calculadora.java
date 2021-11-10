872c84cdcc800a539a1a2f1fdb3d149d23f48717
JCarlosVico <jfervic933@maralboran.es>
2021-11-10 13:34:34 +0100

    Operaciones de cálculo

--- src/main/java/calculator/Calculadora.java
+++ src/main/java/calculator/Calculadora.java
@@ -43,9 +43,8 @@ public class Calculadora {
 //            } while (solicitarDatos); // Bucle que controla la excepción
 //        } while (!((operando1 > -100 && operando1 < 100)
 //                && (operando2 > -100 && operando2 < 100)));
-
         do {
-           
+
             try {
 
                 // Bloque de código candidato a lanzar la excepción
@@ -53,12 +52,12 @@ public class Calculadora {
                 operando1 = sc.nextInt();
                 System.out.println("Introduce operando 2: ");
                 operando2 = sc.nextInt();
-                if ((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)){
-                    solicitarDatos = false;  
-                } else{
+                if ((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)) {
+                    solicitarDatos = false;
+                } else {
                     System.out.println("Los datos están fuera de rango");
                 }
-                
+
             } catch (InputMismatchException ime) {
                 // Código para tratar el error
                 System.out.println("Se ha introducido texto en lugar de números. "
@@ -66,30 +65,33 @@ public class Calculadora {
                 sc.nextLine();
             }
         } while (solicitarDatos);
-        
-       
-        
+
         sc.nextLine();
         String opcion = "";
-        do{
-            
+        int resultado = 0;
+        do {
+
             System.out.println("Opciones del programa");
             System.out.println("1 - Sumar");
             System.out.println("2 - Restar");
             System.out.println("3 - Salir");
             opcion = sc.nextLine();
-            
-            switch(opcion){
+
+            switch (opcion) {
                 case "1":
+                    resultado = operando1 + operando2;
+                    
                     break;
                 case "2":
+                    resultado = operando1 - operando2;
+                    
                     break;
                 default:
                     System.out.println("Opción no válida");
             }
-            
-        } while(!opcion.equals("3"));
-        
+            System.out.println("El resultado es " + resultado);
+        } while (!opcion.equals("3"));
+
     }
 
 }
